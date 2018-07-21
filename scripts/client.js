76e3ac3bdddb71b4eb2e354b36000b5da1580b21
ddmills <mills.dalton@gmail.com>
2018-07-21 11:49:58 -0500

    add build script

--- scripts/client.js
+++ scripts/client.js
@@ -2,11 +2,13 @@ const Bundler = require('parcel-bundler');
 const path = require('path');
 
 const file = path.join(__dirname, '..', 'src', 'client', 'index.html');
+
 const options = {
   target: 'browser',
   outDir: path.join(__dirname, '..', 'dist', 'client'),
   cacheDir: path.join(__dirname, '..', '.cache', 'client'),
   publicUrl: './client',
+  watch: process.argv.includes('watch'),
 };
 
 const bundler = new Bundler(file, options);
--- scripts/server.js
+++ scripts/server.js
@@ -3,10 +3,13 @@ const childProcess = require('child_process');
 const path = require('path');
 
 const file = path.join(__dirname, '..', 'src', 'server', 'index.js');
+const watch = process.argv.includes('watch');
+
 const options = {
   target: 'node',
   outDir: path.join(__dirname, '..', 'dist', 'server'),
   cacheDir: path.join(__dirname, '..', '.cache', 'server'),
+  watch,
 };
 
 const bundler = new Bundler(file, options);
@@ -19,28 +22,30 @@ bundler.on('bundled', (compiledBundle) => {
 });
 
 bundler.on('buildEnd', () => {
-  if (bundle !== null) {
-    if (child) {
-      child.stdout.removeAllListeners('data');
-      child.stderr.removeAllListeners('data');
-      child.removeAllListeners('exit');
-      child.kill();
-    }
-    child = childProcess.spawn('node', [bundle.name]);
-
-    child.stdout.on('data', (data) => {
-      process.stdout.write(data);
-    });
-
-    child.stderr.on('data', (data) => {
-      process.stdout.write(data);
-    });
-
-    child.on('exit', (code) => {
-      console.log(`Child process exited with code ${code}`); // eslint-disable-line no-console
-      child = null;
-    });
+  if (!bundle || !watch) {
+    return;
   }
+
+  if (child) {
+    child.stdout.removeAllListeners('data');
+    child.stderr.removeAllListeners('data');
+    child.removeAllListeners('exit');
+    child.kill();
+  }
+  child = childProcess.spawn('node', [bundle.name]);
+
+  child.stdout.on('data', (data) => {
+    process.stdout.write(data);
+  });
+
+  child.stderr.on('data', (data) => {
+    process.stdout.write(data);
+  });
+
+  child.on('exit', (code) => {
+    console.log(`Child process exited with code ${code}`); // eslint-disable-line no-console
+    child = null;
+  });
 
   bundle = null;
 });
