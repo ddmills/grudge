6e345db7640178077413e3521798c71d2b43e523
ddmills <mills.dalton@gmail.com>
2018-08-10 15:15:47 -0500

    move web server

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,12 @@
 import config from 'config';
 import Logger from 'utilities/Logger';
+import WebServer from 'web/WebServer';
 import App from './App';
-import Server from './Server';
 
-Server(App()).listen(config.server.port, () => {
+const app = App();
+const webServer = WebServer(app);
+
+webServer.listen(config.server.port, () => {
   const location = `${config.server.protocol}://${config.server.host}:${config.server.port}`;
 
   Logger.info(`Serving on ${location}`);
