b4b3ef165a67d4e692877166769a6a606d5bf208
ddmills <mills.dalton@gmail.com>
2018-08-18 15:53:25 -0500

    switch from parcel to webpack

---
+++ client/webpack.config.js
@@ -0,0 +1,49 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(css|sass|scss)$/,
+        use: [{
+          loader: 'style-loader',
+        }, {
+          loader: 'css-loader',
+        }, {
+          loader: 'sass-loader',
+          options: {
+            includePaths: [
+              './src/styles',
+            ],
+          },
+        }, {
+          loader: 'postcss-loader',
+        }],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: false,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
+};
---
+++ server/webpack.config.js
@@ -0,0 +1,17 @@
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  externals: [nodeExternals()],
+};
