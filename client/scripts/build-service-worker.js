6dfebda753bdb614ed82cd334a1be8d6acaecaec
ddmills <mills.dalton@gmail.com>
2018-08-18 22:20:18 -0500

    organize scripts

---
+++ client/scripts/build-service-worker.js
@@ -0,0 +1,19 @@
+const { injectManifest } = require('workbox-build');
+
+const buildServiceWorker = () => {
+  injectManifest({
+    swSrc: 'src/client/service-worker.js',
+    swDest: 'dist/client/service-worker.js',
+    globPatterns: [
+      '**/*.{png,html}',
+    ],
+    globDirectory: 'dist/client',
+    modifyUrlPrefix: {
+      '': 'client/',
+    },
+  }).catch((err) => {
+    console.error(err); // eslint-disable-line no-console
+  });
+};
+
+module.exports = buildServiceWorker;
