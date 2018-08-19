3f3bedeb32f071750bc4d26f6d96df377c913ce8
ddmills <mills.dalton@gmail.com>
2018-08-19 10:15:56 -0500

    favicon, manifest, and uglify

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,9 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const path = require('path');
 
 const isDebug = process.env.NODE_ENV === 'development';
 
@@ -8,10 +11,15 @@ module.exports = {
   output: {
     publicPath: '/client'
   },
+  devtool: 'eval-source-map',
   optimization: {
     minimizer: [
+      new UglifyJsPlugin(),
       new OptimizeCSSAssetsPlugin(),
     ],
+  },
+  stats: {
+    children: false,
   },
   module: {
     rules: [
@@ -62,7 +70,29 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: './src/index.html',
       filename: './index.html',
+      favicon: path.resolve('src/meta/favicon.ico'),
     }),
     new MiniCssExtractPlugin(),
+    new WebpackPwaManifestPlugin({
+      name: 'Otter',
+      short_name: 'Otter',
+      start_url: '.',
+      background_color: '#bee9ff',
+      lang: 'en-US',
+      scope: '/',
+      theme_color: '#ff6347',
+      icons: [
+        {
+          src: path.resolve('src/meta/icons/icon-128x128.png'),
+          sizes: '128x128',
+          type: 'image/png',
+        },
+        {
+          src: path.resolve('src/meta/icons/icon-512x512.png'),
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+    }),
   ],
 };
