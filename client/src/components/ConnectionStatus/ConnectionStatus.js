55c22d03511391f70f400ddadc94f19cae1e4f03
ddmills <mills.dalton@gmail.com>
2018-08-20 18:11:06 -0500

    Show socket connection status

---
+++ client/src/components/ConnectionStatus/ConnectionStatus.js
@@ -0,0 +1,38 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import connect from 'utilities/mobx/Connect';
+import classNames from 'classnames';
+import styles from './ConnectionStatus.scss';
+
+@connect(({ socketStore }) => ({
+  isConnected: socketStore.isConnected,
+  isConnecting: socketStore.isConnecting,
+}))
+export default class ConnectionStatus extends Component {
+  static propTypes = {
+    isConnected: PropTypes.bool,
+    isConnecting: PropTypes.bool,
+  }
+
+  static defaultProps = {
+    isConnected: false,
+    isConnecting: false,
+  }
+
+  render() {
+    const {
+      isConnected,
+      isConnecting,
+    } = this.props;
+
+    const classes = classNames(styles.status, {
+      [styles.connecting]: isConnecting,
+      [styles.connected]: isConnected,
+      [styles.disconnected]: !isConnected && !isConnecting,
+    });
+
+    return (
+      <span className={classes}/>
+    );
+  }
+}
