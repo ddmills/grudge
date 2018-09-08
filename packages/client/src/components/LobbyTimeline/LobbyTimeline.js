e0a2c85835f525bc7714456de8f2d721f7e3800c
ddmills <mills.dalton@gmail.com>
2018-09-08 16:33:36 -0500

    Add End turn button

--- packages/client/src/components/LobbyTimeline/LobbyTimeline.js
+++ packages/client/src/components/LobbyTimeline/LobbyTimeline.js
@@ -1,23 +1,27 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Container } from '@grudge/components';
+import { Container, Button } from '@grudge/components';
 import connect from 'utilities/mobx/Connect';
 import styles from './LobbyTimeline.scss';
 
 @connect(({ lobbyStore }) => ({
   percentTurnTimeRemaining: lobbyStore.turnTimer.percentRemaining,
+  endTurn: lobbyStore.endTurn,
 }))
 export default class LobbyTimeline extends Component {
   static propTypes = {
     percentTurnTimeRemaining: PropTypes.number,
+    endTurn: PropTypes.func,
   };
 
   static defaultProps = {
     percentTurnTimeRemaining: 100,
+    endTurn: undefined,
   };
 
   render() {
     const {
+      endTurn,
       percentTurnTimeRemaining,
     } = this.props;
 
@@ -27,6 +31,9 @@ export default class LobbyTimeline extends Component {
       <div className={styles.lobbyTimelineBanner}>
         <Container className={styles.lobbyTimeline} isPadded={false}>
           <div className={styles.turnProgress} style={{ width }}/>
+          <Button size="sm" color="primary" isDisabled={!endTurn} onClick={endTurn} className={styles.endTurnButton}>
+            End turn
+          </Button>
         </Container>
       </div>
     );
--- packages/client/src/screens/Lobby/LobbyGame.js
+++ packages/client/src/screens/Lobby/LobbyGame.js
@@ -1,30 +1,12 @@
 import { Component } from 'react';
-import PropTypes from 'prop-types';
-import connect from 'utilities/mobx/Connect';
 import { Container } from '@grudge/components';
-import { Lobby } from '@grudge/domain';
 import PageSuperHeader from 'components/Page/SuperHeader/PageSuperHeader';
 import LobbyGameHeader from 'components/LobbyGameHeader/LobbyGameHeader';
 import LobbyTimeline from 'components/LobbyTimeline/LobbyTimeline';
 import styles from './LobbyGame.scss';
 
-@connect(({ lobbyStore }) => ({
-  lobby: lobbyStore.lobby,
-}))
 export default class LobbyGame extends Component {
-  static propTypes = {
-    lobby: PropTypes.instanceOf(Lobby),
-  }
-
-  static defaultProps = {
-    lobby: null,
-  }
-
   render() {
-    const {
-      lobby,
-    } = this.props;
-
     return (
       <section className={styles.fullPage}>
         <PageSuperHeader/>
