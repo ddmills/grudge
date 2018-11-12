e3906c2c20d41d08e9b8a9cb21a0982934dd30e5
ddmills <mills.dalton@gmail.com>
2018-11-11 18:53:37 -0600

    update remaining effects

--- packages/domain/interpreters/ContextAdministrator.js
+++ packages/domain/interpreters/ContextAdministrator.js
@@ -1,3 +1,5 @@
+import { CardLocations } from '@grudge/data';
+import clone from 'lodash.clonedeep';
 import ContextInterrogator from './ContextInterrogator';
 
 export default class ContextAdministrator {
@@ -29,6 +31,25 @@ export default class ContextAdministrator {
     card.isDisabled = false;
   }
 
+  static playCard(ctx, cardId, slotIndex) {
+    const card = ContextInterrogator.getCard(ctx, cardId);
+
+    card.slotIndex = slotIndex;
+  }
+
+  static trashCard(ctx, cardId) {
+    const card = ContextInterrogator.getCard(ctx, cardId);
+
+    card.slotIndex = null;
+    card.location = CardLocations.TRASH;
+  }
+
+  static setMoneyForPlayer(ctx, playerId, value) {
+    const player = ContextInterrogator.getPlayer(ctx, playerId);
+
+    player.money = value;
+  }
+
   static addMoneyToPlayer(ctx, playerId, amount) {
     const player = ContextInterrogator.getPlayer(ctx, playerId);
 
@@ -42,10 +63,28 @@ export default class ContextAdministrator {
     player.money = newMoney > 0 ? newMoney : 0;
   }
 
-  static killCard(ctx, cardId) {
+  static setHealthForPlayer(ctx, playerId, value) {
+    const player = ContextInterrogator.getPlayer(ctx, playerId);
+
+    player.health = value;
+  }
+
+  static subtractHealthFromPlayer(ctx, playerId, amount) {
+    const player = ContextInterrogator.getPlayer(ctx, playerId);
+    const newHealth = player.health - amount;
+
+    player.health = newHealth > 0 ? newHealth : 0;
+  }
+
+  static resetCard(ctx, cardId) {
     const cardType = ContextInterrogator.getCardTypeForCard(ctx, cardId);
     const card = ContextInterrogator.getCard(ctx, cardId);
 
-
+    card.location = CardLocations.DECK;
+    card.slotIndex = null;
+    card.isDisabled = false;
+    card.playActions = clone(cardType.playActions);
+    card.handActions = clone(cardType.handActions);
+    card.traits = clone(cardType.traits);
   }
 }
--- packages/server/src/actions/effects/TrashEffect.js
+++ packages/server/src/actions/effects/TrashEffect.js
@@ -1,22 +1,14 @@
+import { ContextAdministrator } from '@grudge/domain/interpreters';
 import { EffectIds } from '@grudge/data';
-import CardRepository from 'repositories/CardRepository';
-import LobbyRepository from 'repositories/LobbyRepository';
 import NotificationService from 'services/NotificationService';
 import Effect from './Effect';
 
 export default class TrashEffect extends Effect {
   static id = EffectIds.TRASH;
 
-  static async apply(effectParams, { card, user }) {
-    const trashedCard = card.clone({
-      isTrashed: true,
-      slotIndex: null,
-    });
+  static async apply(ctx, effectParams, { cardId }) {
+    ContextAdministrator.trashCard(ctx, cardId);
 
-    await CardRepository.save(trashedCard);
-
-    const lobby = await LobbyRepository.get(user.lobbyId);
-
-    NotificationService.onCardTrashed(lobby, trashedCard);
+    NotificationService.onCardTrashed(ctx, cardId);
   }
 }
